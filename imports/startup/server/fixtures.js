b63187d9de4a89193c0fa39307f36859a6a253ec
Ubuntu <ubuntu@ip-172-31-26-51.ap-southeast-1.compute.internal>
2019-12-03 11:49:13 +0000

    astronomy is crap

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,10 +1,32 @@
 // Fill the DB with example data on startup
 
 import { Meteor } from 'meteor/meteor';
-import {ImagesFiles} from '../../api/images/images.js'
+import { ImagesFiles, Products } from '../../api/cols.js'
 
-if (ImagesFiles.find().count() == 0){
-     ImagesFiles.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
-      fileName: 'logo.png'
-    }); 
-}
+ImagesFiles.remove({ "meta.productId": "abcd" }, () => {
+    ImagesFiles.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
+        fileName: 'logo.png',
+        meta: {
+            productId: "abcd"
+        }
+    }, function(er, fileRef) {
+        console.log(er, "cant load the mock product")
+        if (!Products.findOne('abcd')) {
+            Products.insert({
+                price: 23,
+                title: 'The abcd',
+                description: 'mocked product',
+                imageIds: [fileRef._id]
+            })
+        }
+        else {
+            Products.update({
+                title: 'The abcd'
+            }, {
+                imageIds: [fileRef._id]
+            })
+        }
+    });
+
+
+})
